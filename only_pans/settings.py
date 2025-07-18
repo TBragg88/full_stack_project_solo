ce938d75768535707eefe1bd8202c4beadf18abc
Tom Bragg <tbragg88@hotmail.com>
2025-07-19 00:29:06 +0100

    Add Cloudinary integration and create initial migrations for user profiles, recipes, search history, comments, ratings, and tags

--- only_pans/settings.py
+++ only_pans/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'accounts',
     'recipes',
     'tags',
